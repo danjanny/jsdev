75248638cb58dd9768490ae7c2f428d0af137280
ridha <ridha@komunigrafik.com>
2018-08-12 08:55:14 +0700

    commit

--- src/like_button.js
+++ src/like_button.js
@@ -1,24 +1,2 @@
-class LikeButton extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            liked: false
-        };
-    }
-
-    render() {
-        if(this.state.liked) {
-            let person = new Person('Ridha Danjanny');
-            return person.getName();
-        }
-
-        return (
-            <button onClick={ () => this.setState({liked: true}) }>Like</button>
-        );
-    }   
-}
-
-
 let domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(<LikeButton/>, domContainer);
+ReactDOM.render(<h1>Ridha Danjanny</h1>, domContainer);
